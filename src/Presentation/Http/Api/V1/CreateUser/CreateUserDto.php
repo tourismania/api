c0ac99939d2bbf3b55f58c35554f5e4401da136d
kirin <kirin.mzzt@gmail.com>
2025-11-06 18:30:23 +0300

    Добавлена валидация на уровне Request

--- src/Presentation/Http/Api/V1/CreateUser/CreateUserDto.php
+++ src/Presentation/Http/Api/V1/CreateUser/CreateUserDto.php
@@ -6,17 +6,21 @@ declare(ticks=1000);
 namespace App\Presentation\Http\Api\V1\CreateUser;
 
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 final readonly class CreateUserDto
 {
     public function __construct(
         #[SerializedName('first_name')]
+        #[Assert\NotBlank(message: 'Имя должно быть указано')]
         public string $firstName,
 
         #[SerializedName('last_name')]
+        #[Assert\NotBlank(message: 'Фамилия должна быть указана')]
         public string $lastName,
 
         #[SerializedName('email')]
+        #[Assert\NotBlank(message: 'Почта должна быть указана')]
         public string $email,
     )
     {
